fef3e40c5ba7f17071a9cd7111a4292d36f93718
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2026-09-21 20:54:53 +0200

    Ejercicios bucles PHP

--- PHP/ejercicios/ejercicio02.php
+++ PHP/ejercicios/ejercicio02.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<?php
+$num1 = 7;
+?>
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tabla de multiplicar (en tabla) del número <?= $num1 ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
+
 <body>
 
-    
+    <div class="container">
+        <h1>Tabla de multiplicar (en tabla) del número <?= $num1 ?></h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Primer número</th>
+                    <th scope="col">Segundo número</th>
+                    <th scope="col">Producto</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php for ($num2 = 0; $num2 <= 10; $num2++): ?>
+                    <tr>
+                        <td><?= $num1 ?></td>
+                        <td><?= $num2 ?></td>
+                        <td><?= $num1 * $num2 ?></td>
+                    </tr>
+                <?php endfor ?>
+
+
+            </tbody>
+
+            <!-- <tfoot><tfoot> -->
+        </table>
+
+    </div>
 </body>
+
 </html>
---
+++ PHP/ejercicios/ejercicio03.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<?php
+$minNumber = 3;
+$maxNumber = 9;
+?>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de multiplicar de los números del <?= $minNumber ?> al <?= $maxNumber ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Tabla de multiplicar de los números del <?= $minNumber ?> al <?= $maxNumber ?></h1>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col"><!--Vacía--></th>
+                    <?php for ($num1 = $minNumber; $num1 <= $maxNumber; $num1++): ?>
+                        <th scope="col"><?= $num1 ?></th>
+                    <?php endfor ?>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php for ($num2 = 0; $num2 <= 10; $num2++): ?>
+                    <tr>
+                        <th scope="row"><?= $num2 ?></th>
+                        <?php for ($num1 = $minNumber; $num1 <= $maxNumber; $num1++): ?>
+                            <th scope="col"><?= $num1 * $num2 ?></th>
+                        <?php endfor ?>
+                    </tr>
+                <?php endfor ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
